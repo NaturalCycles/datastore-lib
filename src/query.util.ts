8e36412abbc15ed6ec0132124fa811b8393de06f
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-10-25 17:29:25 +0100

    fix: adapt to recent db-lib (map '==' operator to '=')

--- src/query.util.ts
+++ src/query.util.ts
@@ -1,15 +1,21 @@
 import { Query } from '@google-cloud/datastore'
-import { DBQuery } from '@naturalcycles/db-lib'
+import { DBQuery, DBQueryFilterOperator } from '@naturalcycles/db-lib'
+import { StringMap } from '@naturalcycles/js-lib'
+// import Operator = google.datastore.v1.CompositeFilter.Operator
 
-const FNAME_MAP: Record<string, string | undefined> = {
+const FNAME_MAP: StringMap = {
   id: '__key__',
+}
+
+const OP_MAP: Partial<Record<DBQueryFilterOperator, string>> = {
+  '==': '=',
 }
 
 export function dbQueryToDatastoreQuery(dbQuery: Readonly<DBQuery>, emptyQuery: Query): Query {
   let q = emptyQuery
 
   // filter
-  q = dbQuery._filters.reduce((q, f) => q.filter(f.name, f.op as any, f.val), q)
+  q = dbQuery._filters.reduce((q, f) => q.filter(f.name, OP_MAP[f.op] || (f.op as any), f.val), q)
 
   // limit
   q = q.limit(dbQuery._limitValue || 0)
