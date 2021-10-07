419f8e16ccb9722771a01b90001ae19f2a7a8595
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2021-10-07 02:16:54 +0200

    feat: experimentalCursorStream mode to fix backpressure
    Works around this issue: https://github.com/googleapis/nodejs-datastore/issues/859
    by implementing a custom Readable that respects backpressure.
    To not get too slow, allows to set `rssLimitMB`.
    Until `rssLimitMB` is reached - it works like the official library ("downloads whole internet"),
    but gently pauses as soons as `rssLimitMB` is reached.

---
+++ scripts/oom.script.ts
@@ -0,0 +1,85 @@
+/*
+
+yarn tsn oom
+
+ */
+
+import { Transform, Writable } from 'stream'
+import { DBQuery } from '@naturalcycles/db-lib'
+import { _pipeline, requireEnvKeys, transformLogProgress } from '@naturalcycles/nodejs-lib'
+import { runScript } from '@naturalcycles/nodejs-lib/dist/script'
+import { DatastoreDB } from '../src'
+
+const { SECRET_GCP_SERVICE_ACCOUNT } = requireEnvKeys('SECRET_GCP_SERVICE_ACCOUNT')
+process.env['APP_ENV'] = 'master'
+
+const db = new DatastoreDB({
+  credentials: JSON.parse(SECRET_GCP_SERVICE_ACCOUNT),
+  useLegacyGRPC: true,
+})
+
+// const TABLE = 'TEST1'
+const TABLE = 'UserFertilityCache'
+
+runScript(async () => {
+  // prepare
+  // const items = _range(0, 10_000).map(i => ({
+  //   id: `id_${i}`,
+  //   data: Buffer.alloc(1_000_000).fill(1),
+  // }))
+  //
+  // console.log('items ok')
+  //
+  // await _pipeline([
+  //   Readable.from(items),
+  //   transformMap(async item => {
+  //     await db.saveBatch(TABLE, [item], {
+  //       excludeFromIndexes: ['data'],
+  //     })
+  //   }, {
+  //     concurrency: 16,
+  //     predicate: _passthroughPredicate,
+  //   }),
+  //   transformLogProgress({logEvery: 10}),
+  //   writableVoid(),
+  // ])
+
+  await _pipeline([
+    db.streamQuery(DBQuery.create(TABLE), {
+      experimentalCursorStream: true,
+      batchSize: 1000,
+      rssLimitMB: 1000,
+      debug: true,
+    }),
+
+    // This thing logs every 100's object + some memory and speed metrics
+    transformLogProgress({
+      metric: 'read',
+      logEvery: 1000,
+      peakRSS: true,
+    }),
+
+    // This is a fake Consumer that introduces a 100ms delay before passing the data further
+    new Transform({
+      objectMode: true,
+      transform(chunk: any, _encoding, cb) {
+        setTimeout(() => cb(null, chunk), 5)
+      },
+    }),
+
+    transformLogProgress({
+      metric: 'processed',
+      logEvery: 1000,
+      peakRSS: true,
+    }),
+
+    // This is a "no-op" consumer that just discards the data
+    new Writable({
+      objectMode: true,
+      write(c, _encoding, cb) {
+        // console.log(c.id, c.data.length)
+        cb()
+      },
+    }),
+  ])
+})
